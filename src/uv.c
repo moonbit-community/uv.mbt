31541a8e7a8a2d3345147d8aa857310108d553e5
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-06-25 09:47:29 +0800

    feat: cpu_info()

--- src/uv.c
+++ src/uv.c
@@ -3919,3 +3919,60 @@ moonbit_uv_cancel(uv_req_t *req) {
   moonbit_decref(req);
   return result;
 }
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_uv_cpu_info(uv_cpu_info_t **cpu_infos, int32_t *count) {
+  int32_t result = uv_cpu_info(cpu_infos, count);
+  moonbit_decref(cpu_infos);
+  moonbit_decref(count);
+  return result;
+}
+
+MOONBIT_FFI_EXPORT
+const char *
+moonbit_uv_cpu_info_get_model(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].model;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_uv_cpu_info_get_speed(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].speed;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_cpu_info_get_cpu_times_user(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].cpu_times.user;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_cpu_info_get_cpu_times_nice(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].cpu_times.nice;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_cpu_info_get_cpu_times_sys(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].cpu_times.sys;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_cpu_info_get_cpu_times_idle(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].cpu_times.idle;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_cpu_info_get_cpu_times_irq(uv_cpu_info_t *cpu_info, int32_t i) {
+  return cpu_info[i].cpu_times.irq;
+}
+
+MOONBIT_FFI_EXPORT
+void
+moonbit_uv_free_cpu_info(uv_cpu_info_t *cpu_info, int32_t count) {
+  uv_free_cpu_info(cpu_info, count);
+}
