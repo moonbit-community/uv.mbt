8b6e082713ecf05cc91c69b81d64886a20e8def4
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-07-06 17:02:39 +0800

    feat: metrics

--- src/uv.c
+++ src/uv.c
@@ -4103,3 +4103,50 @@ moonbit_uv_write2(
   moonbit_decref(send_handle);
   return result;
 }
+
+MOONBIT_FFI_EXPORT
+uv_metrics_t *
+moonbit_uv_metrics_make(void) {
+  return (uv_metrics_t *)moonbit_make_bytes(sizeof(uv_metrics_t), 0);
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_metrics_get_loop_count(uv_metrics_t *metrics) {
+  uint64_t count = metrics->loop_count;
+  moonbit_decref(metrics);
+  return count;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_metrics_get_events(uv_metrics_t *metrics) {
+  uint64_t events = metrics->events;
+  moonbit_decref(metrics);
+  return events;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_metrics_get_events_waiting(uv_metrics_t *metrics) {
+  uint64_t events_waiting = metrics->events_waiting;
+  moonbit_decref(metrics);
+  return events_waiting;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_uv_metrics_info(uv_loop_t *loop, uv_metrics_t *metrics) {
+  int32_t status = uv_metrics_info(loop, metrics);
+  moonbit_decref(loop);
+  moonbit_decref(metrics);
+  return status;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_metrics_idle_time(uv_loop_t *loop) {
+  uint64_t idle_time = uv_metrics_idle_time(loop);
+  moonbit_decref(loop);
+  return idle_time;
+}
