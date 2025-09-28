b6d2dbaff7b41b33073d64827d0b4028c1445496
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-09-28 13:21:54 +0800

    feat: group

--- src/passwd.c
+++ src/passwd.c
@@ -102,3 +102,67 @@ moonbit_uv_passwd_get_shell(moonbit_uv_passwd_t *passwd) {
     return moonbit_empty_int8_array;
   }
 }
+
+typedef struct moonbit_uv_group_s {
+  uv_group_t group;
+} moonbit_uv_group_t;
+
+static inline void
+moonbit_uv_os_free_group(void *object) {
+  moonbit_uv_group_t *group = (moonbit_uv_group_t *)object;
+  uv_os_free_group(&group->group);
+}
+
+MOONBIT_FFI_EXPORT
+moonbit_uv_group_t *
+moonbit_uv_group_make(void) {
+  moonbit_uv_group_t *group = moonbit_make_external_object(
+    moonbit_uv_os_free_group, sizeof(moonbit_uv_group_t)
+  );
+  memset(group, 0, sizeof(moonbit_uv_group_t));
+  return group;
+}
+
+MOONBIT_FFI_EXPORT
+moonbit_bytes_t
+moonbit_uv_group_get_groupname(moonbit_uv_group_t *group) {
+  size_t length = strlen(group->group.groupname);
+  moonbit_bytes_t groupname = moonbit_make_bytes(length, 0);
+  memcpy(groupname, group->group.groupname, length);
+  moonbit_decref(group);
+  return groupname;
+}
+
+MOONBIT_FFI_EXPORT
+uint64_t
+moonbit_uv_group_get_gid(moonbit_uv_group_t *group) {
+  uint64_t gid = group->group.gid;
+  moonbit_decref(group);
+  return gid;
+}
+
+MOONBIT_FFI_EXPORT
+moonbit_bytes_t *
+moonbit_uv_group_get_members(moonbit_uv_group_t *group) {
+  size_t count = 0;
+  while (group->group.members[count]) {
+    count++;
+  }
+  moonbit_bytes_t *members =
+    (moonbit_bytes_t *)moonbit_make_ref_array(count, NULL);
+  for (size_t i = 0; i < count; i++) {
+    size_t length = strlen(group->group.members[i]);
+    members[i] = moonbit_make_bytes(length, 0);
+    memcpy(members[i], group->group.members[i], length);
+  }
+  moonbit_decref(group);
+  return members;
+}
+
+MOONBIT_FFI_EXPORT
+int32_t
+moonbit_uv_os_get_group(moonbit_uv_group_t *group, uint64_t gid) {
+  int32_t result = uv_os_get_group(&group->group, gid);
+  moonbit_decref(group);
+  return result;
+}
