cb87af61e0a3c60d3a4061b36b3a30328984e96c
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-09-28 10:37:52 +0800

    feat: add write to format.py

--- scripts/format.py
+++ scripts/format.py
@@ -15,46 +15,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+import argparse
 from pathlib import Path
 import subprocess
 import logging
 import json
 
 
-def clang_format():
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def clang_format(write: bool = False):
+    logger.info(f"formatting C files: write = {write}")
     files = []
     for path in Path("src").glob("*.[ch]"):
         if path.is_file() and not path.match("uv#*"):
             files.append(str(path))
-            logging.info(f"file {path} will be formatted")
-    subprocess.run(["clang-format", "--dry-run", "--Werror", *files], check=True)
+            logger.info(f"file {path} will be checked")
+    if write:
+        subprocess.run(["clang-format", "-i", *files], check=True)
+    else:
+        subprocess.run(["clang-format", "--dry-run", "--Werror", *files], check=True)
 
 
-def json_format():
-    files: list[Path] = []
-    for path in Path(".").rglob("moon.pkg.json"):
-        if path.is_file():
-            files.append(path)
-            logging.info(f"file {path} will be formatted")
-    for path in Path(".").rglob("moon.mod.json"):
-        if path.is_file():
-            files.append(path)
-            logging.info(f"file {path} will be formatted")
+def json_format(write: bool = False):
+    logger.info(f"formatting JSON files: write = {write}")
+    files: list[Path] = [Path("src/moon.pkg.json"), Path("moon.mod.json")]
     for path in files:
-        path.write_text(
-            json.dumps(json.loads(path.read_text(encoding="utf-8")), indent=2) + "\n",
-            encoding="utf-8",
-        )
+        logger.info(f"file {path} will be checked")
+        text = path.read_text(encoding="utf-8")
+        parsed = json.loads(text)
+        formatted = json.dumps(parsed, indent=2, sort_keys=True) + "\n"
+        if text != formatted:
+            if write:
+                path.write_text(formatted, encoding="utf-8")
+            else:
+                raise ValueError(f"File {path} is not properly formatted")
 
 
-def moon_fmt():
-    subprocess.run(["moon", "fmt", "--check"], check=True)
+def moon_fmt(write: bool = False):
+    logger.info(f"formatting MoonBit files: write = {write}")
+    if write:
+        subprocess.run(["moon", "fmt"], check=True)
+    else:
+        subprocess.run(["moon", "fmt", "--check"], check=True)
 
 
 def main():
-    clang_format()
-    json_format()
-    moon_fmt()
+    parser = argparse.ArgumentParser(description="Check code format")
+    parser.add_argument(
+        "--write",
+        action="store_true",
+        default=False,
+        help="Write changes to files",
+    )
+    args = parser.parse_args()
+    print(args)
+    if args.write:
+        logger.info("Writing code format changes")
+    else:
+        logger.info("Checking code format")
+    clang_format(write=args.write)
+    json_format(write=args.write)
+    moon_fmt(write=args.write)
 
 
 if __name__ == "__main__":
